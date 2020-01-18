bf1bf3ca1f78745e93f9b71326a70cd1dc56708c
Rodrigo Segui <46058568+Rodrigo-Segui@users.noreply.github.com>
2020-01-17 22:40:36 -0300

    3 aula

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,13 @@
 const { Router } = require('express');
+const DevController = require('./controllers/DevController');
+const SearchController = require('./controllers/SearchController');
+
 const routes = Router();
-routes.post('/devs', (request, response) => {
-    const {github_username } = request.body;
-    return response.json({ message : 'Hello Omnistack'});
-});
+
+routes.get('/devs', DevController.index)
+routes.post('/devs', DevController.store);
+
+routes.get('/search', SearchController.index)
 
 
 module.exports = routes;
